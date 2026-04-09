fae670f2371ba4b60b585bffeeaa166eaa6e32ab
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-04-09 13:37:15 -0600

    programa2.c

---
+++ programa2.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+int main() 
+{
+   /* Al igual que en la estructura if-else, 0 -> falso y diferente de 0 -> verdadero. El siguiente es un ciclo infinito porque la condición siempre es verdadera.
+    Así mismo, debido a que el ciclo consta de una sola línea, las llaves { } son opcionales.*/
+  
+  while (100) 
+  {
+  printf("Ciclo infinito.\nPara terminar el ciclo presione ctrl + c.\n"); 
+  }
+  return 0;
+  }
